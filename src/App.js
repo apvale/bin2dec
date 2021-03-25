fb5bdff688ecbcc70d701be4bcd6c9ef4355be49
Ana Paula Vale <ana.vale@talkdesk.com>
2021-03-25 10:05:20 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useState, createRef} from 'react'
 
-function App() {
+const App = () => {
+  const [result, setResult] = useState(0)
+  const inputRef = createRef()
+
+  const handleConvert = () => {
+    setResult(parseInt(inputRef.current.value, 2))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="binary-area">
+        <h2>Bin2Dec</h2>
+        <input ref={inputRef}></input>
+        <button onClick={handleConvert}>Convert</button>
+        <h1>{result}</h1>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
