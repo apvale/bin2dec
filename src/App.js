b7395e69039659d3f4254935cf5a636443479260
Ana Paula Vale <ana.vale@talkdesk.com>
2021-03-28 15:22:57 +0100

    feat(App): finish app behavior

--- src/App.js
+++ src/App.js
@@ -2,20 +2,34 @@ import './App.css'
 import { useState, createRef } from 'react'
 
 const App = () => {
-  const [result, setResult] = useState(0)
+  const [result, setResult] = useState({ value: '0', isValid: true })
   const inputRef = createRef()
 
   const handleConvert = () => {
-    setResult(parseInt(inputRef.current.value, 2))
+    const regex = /^[0|1]+$/
+
+    if (regex.exec(inputRef.current.value)) {
+      setResult({
+        value: parseInt(inputRef.current.value, 2),
+        isValid: true
+      })
+    } else {
+      setResult({
+        value: 'Non binary number',
+        isValid: false
+      })
+    }
   }
 
   return (
     <div className='container'>
       <div className='binary-area'>
-        <h2>Bin2Dec</h2>
-        <input ref={inputRef} />
-        <button onClick={handleConvert}>Convert</button>
-        <h1>{result}</h1>
+        <h1>Bin2Dec</h1>
+        <div className='input-area'>
+          <input className={result.isValid ? '' : 'invalid'} ref={inputRef} />
+          <button onClick={handleConvert}>Convert</button>
+          <h1 className={result.isValid ? '' : 'invalid'}>{result.value}</h1>
+        </div>
       </div>
     </div>
   )
